3d6c965ba46120f48a909acada8b2822da5392c9
dkandr <92881116+dkandr@users.noreply.github.com>
2022-04-28 22:30:34 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <future>
+#include <memory>
+#include <chrono>
+#include <random>
+#include "optimized_thread.h"
+
+using namespace std;
+using namespace std::chrono;
+
+bool make_thread = true;
+
+RequestHandler pool;
+	
+void merge(std::shared_ptr<std::promise<void>> spPromise, int* arr, int l, int m, int r) {
+	int nl = m - l + 1;
+	int nr = r - m;
+
+	int* left = new int[nl];
+	int* right = new int[nr];
+
+	for (int i = 0; i < nl; i++)
+		left[i] = arr[l + i];
+	for (int j = 0; j < nr; j++)
+		right[j] = arr[m + 1 + j];
+
+	long i = 0, j = 0;
+	long k = l;
+
+	while (i < nl && j < nr) {
+		if (left[i] <= right[j]) {
+			arr[k] = left[i];
+			i++;
+		}
+		else {
+			arr[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < nl) 	{
+		arr[k++] = left[i++];
+	}
+	while (j < nr) 	{
+		arr[k++] = right[j++];
+	}
+
+	spPromise->set_value();
+
+	delete[] left;
+	delete[] right;
+}
+
+void mergeSort(int* arr, int l, int r) {
+
+	std::promise<void> p;
+
+	std::shared_ptr<std::promise<void>> spPromise = std::make_shared<std::promise<void>>(p);
+	std::future<void> f = p.get_future();
+
+	if (l >= r) {
+		// p.set_value();
+		return;
+	}
+
+	long m = (l + r - 1) / 2;
+
+	mergeSort(arr, l, m);
+	mergeSort(arr, m + 1, r);
+
+	if (make_thread && (m - l > 100000)) {
+		pool.pushRequest(merge, spPromise, arr, l, m, r);
+	} else {
+		merge(spPromise, arr, l, m, r);
+	}
+
+	f.get();
+}
+
+void showArray(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << array[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void checkSort(int* array, int size) {
+	bool sort = true;
+	for (int i = 0; i < size - 1; i++) {
+		if (array[i] > array[i+1]) {
+			sort = false;
+			break;
+		}
+	}
+	std::cout << "Array sorted : " << (sort ? "true" : "false") << std::endl;
+}
+
+int main() {
+	srand(time(NULL));
+	int arr_size = 1000000;                    
+	// int arr_size = 600000;
+	int* array = new int[arr_size];
+
+	for (long i = 0; i < arr_size; i++) {
+		array[i] = rand() % 90 + 10;
+	}
+
+	std::shared_ptr<std::promise<void>> spPromise = std::make_shared<std::promise<void>>();
+	std::future<void> f = spPromise->get_future();
+
+    auto begin = system_clock::now();
+
+	mergeSort(array, 0, arr_size - 1);
+
+    auto end = system_clock::now();
+
+	checkSort(array, arr_size);
+
+    std::cout << "The time: " << duration_cast<milliseconds>(end - begin).count() << " milliseconds" << std::endl << std::endl;
+
+	delete[] array;
+
+	return 0;
+}
---
+++ optimized_thread.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <thread>
+#include <vector>
+#include <queue>
+
+typedef std::function<void()> task_type;
+typedef void (*FuncType) ( std::shared_ptr<std::promise<void>>, int*, int, int, int);
+
+template<class T>
+class BlockedQueue {
+    public:
+    void push(T& item){
+        std::lock_guard<std::mutex> l(m_locker);
+        // обычный потокобезопасный push
+        m_task_queue.push(item);
+        // делаем оповещение, чтобы поток, вызвавший
+        // pop проснулся и забрал элемент из очереди
+        m_notifier.notify_one();
+    }
+    // блокирующий метод получения элемента из очереди
+    void pop(T& item) {
+        std::unique_lock<std::mutex> l(m_locker);
+        if (m_task_queue.empty())
+            // ждем, пока вызовут push
+            m_notifier.wait(l, [this]{return !m_task_queue.empty();});
+        item = m_task_queue.front();
+        m_task_queue.pop();
+    }
+    // неблокирующий метод получения элемента из очереди
+    // возвращает false, если очередь пуста
+    bool fast_pop(T& item) {
+        std::lock_guard<std::mutex> l(m_locker);
+        if(m_task_queue.empty())
+            // просто выходим
+            return false;
+        // забираем элемент
+        item = m_task_queue.front();
+        m_task_queue.pop();
+        return true;
+    }
+
+    private:
+    std::mutex m_locker;
+    // очередь задач
+    std::queue<T> m_task_queue;
+    // уведомитель
+    std::condition_variable m_notifier;
+};
+
+class OptimizeThreadPool {
+   public:
+       OptimizeThreadPool();
+       // запуск:
+       void start();
+       // остановка:
+       void stop();
+       // проброс задач
+       void push_task(FuncType f, std::shared_ptr<std::promise<void>> spPromise, int *arr, int l, int m, int r);
+       // функция входа для потока
+       void threadFunc(int qindex);
+   private:
+       // количество потоков
+       int m_thread_count;
+       // потоки
+       std::vector<std::thread> m_threads;
+       // очереди задач для потоков
+       std::vector<BlockedQueue<task_type>> m_thread_queues;
+       // для равномерного распределения задач
+       int m_index;
+};
+
+class RequestHandler {
+   public:
+       RequestHandler();
+       ~RequestHandler();
+       // отправка запроса на выполнение
+       void pushRequest(FuncType f, std::shared_ptr<std::promise<void>> spPromise, int *arr, int l, int m, int r);
+   private:
+       // пул потоков
+       OptimizeThreadPool m_tpool;
+};
